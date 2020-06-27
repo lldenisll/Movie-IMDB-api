dea8b2d1a9d0f8c472d071cd5039c09ac1c4707c
Denis <namorado@TFGcos-MacBook-Pro.local>
2020-06-27 09:46:41 -0300

    refactoring api

--- main.py
+++ main.py
@@ -17,15 +17,7 @@ data = json.loads(response.content)
 datafinal=(data['Search'][0])
 id = datafinal['imdbID']
 
-
-url = "https://movie-database-imdb-alternative.p.rapidapi.com/"
-
 querystring = {"i":id,"r":"json"}
-
-headers = {
-    'x-rapidapi-host': "movie-database-imdb-alternative.p.rapidapi.com",
-    'x-rapidapi-key': "ac9d9266admshaccf7fc1e5ab755p1de0dajsnd7d6e7d5ef79"
-    }
 
 responser = requests.request("GET", url, headers=headers, params=querystring)
 datar = json.loads(responser.content)
